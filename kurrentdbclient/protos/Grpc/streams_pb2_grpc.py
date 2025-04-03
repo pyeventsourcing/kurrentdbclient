e3d7fff6137f658fd6320582c373cff0292443b7
johnbywater <john.bywater@appropriatesoftware.net>
2025-04-03 10:03:59 +0100

    Adjusted package dependencies to have "grpcio[protobuf]" rather than "grpcio" and "protobuf".
    Also updated packages and rebuilt grpc stubs.

--- kurrentdbclient/protos/Grpc/streams_pb2_grpc.py
+++ kurrentdbclient/protos/Grpc/streams_pb2_grpc.py
@@ -5,7 +5,7 @@ import warnings
 
 from kurrentdbclient.protos.Grpc import streams_pb2 as kurrentdbclient_dot_protos_dot_Grpc_dot_streams__pb2
 
-GRPC_GENERATED_VERSION = '1.68.0'
+GRPC_GENERATED_VERSION = '1.68.1'
 GRPC_VERSION = grpc.__version__
 _version_not_supported = False
 
