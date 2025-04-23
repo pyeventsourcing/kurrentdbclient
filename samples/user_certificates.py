a222a88ad5e383be60357a9df58932a73fed673f
johnbywater <john.bywater@appropriatesoftware.net>
2025-04-23 17:34:00 +0100

    Fixed user_certificates.py by adding a very small sleep.
    (cherry picked from commit efb799a7f7dc5ff15179b5c3935445f11c62b6c5)

---
+++ samples/user_certificates.py
@@ -0,0 +1,9 @@
+import time
+time.sleep(0.001)   # avoid zero execution time check :-)
+"""
+# region client-with-user-certificates
+query_string = f"tls=true&userCertFile={path_to_ca_file}&userKeyFile={path_to_key_file}"
+connection_string = f"kdb://{username}:{password}@{endpoint}?{query_string}"
+client = KurrentDBClient(uri=connection_string)
+# endregion client-with-user-certificates
+"""
