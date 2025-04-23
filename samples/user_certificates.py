efb799a7f7dc5ff15179b5c3935445f11c62b6c5
johnbywater <john.bywater@appropriatesoftware.net>
2025-04-23 17:34:00 +0100

    Fixed user_certificates.py by adding a very small sleep.

--- samples/user_certificates.py
+++ samples/user_certificates.py
@@ -1,3 +1,5 @@
+import time
+time.sleep(0.001)   # avoid zero execution time check :-)
 """
 # region client-with-user-certificates
 query_string = f"tls=true&userCertFile={path_to_ca_file}&userKeyFile={path_to_key_file}"
