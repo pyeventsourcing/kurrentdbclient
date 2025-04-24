ca1cbb0f77a95e9a79c14ea7debe0cc2e58b0b7a
johnbywater <john.bywater@appropriatesoftware.net>
2025-04-24 21:05:28 +0100

    Added async client samples, and updated sync samples to exactly match improved style.

--- samples_async/server_side_filtering.py
+++ samples_async/server_side_filtering.py
@@ -1,37 +1,39 @@
+# ruff: noqa: F704, PLE1142
+import sys
 from uuid import uuid4
 
 from kurrentdbclient import (
+    AsyncCatchupSubscription,
+    AsyncKurrentDBClient,
     Checkpoint,
-    KurrentDBClient,
     NewEvent,
     StreamState,
 )
-from kurrentdbclient.streams import CatchupSubscription, RecordedEvent
+from kurrentdbclient.streams import RecordedEvent
 from tests.test_client import get_server_certificate
 
 DEBUG = False
-_print = print
-
 
 def print(*args):  # noqa: A001
     if DEBUG:
-        _print(*args)
+        sys.stdout.write(" ".join([repr(arg) for arg in args]) + "\n")
 
 
 KDB_TARGET = "localhost:2114"
 qs = "MaxDiscoverAttempts=2&DiscoveryInterval=100&GossipTimeout=1"
 
-client = KurrentDBClient(
+client = AsyncKurrentDBClient(
     uri=f"kdb://admin:changeit@{KDB_TARGET}?{qs}",
     root_certificates=get_server_certificate(KDB_TARGET),
 )
+await client.connect()
 
-subscription: CatchupSubscription
+subscription: AsyncCatchupSubscription
 
 
-def handle_event(ev: RecordedEvent):
+async def handle_event(ev: RecordedEvent):
     print(f"handling event: {ev.stream_position} {ev.type}")
-    subscription.stop()
+    await subscription.stop()
 
 
 stream_name = str(uuid4())
@@ -41,25 +43,27 @@ event_data = NewEvent(
     data=b'{"id": "1", "important_data": "some value"}',
 )
 
-client.append_to_stream(
+await client.append_to_stream(
     stream_name=stream_name,
     current_version=StreamState.ANY,
     events=event_data,
 )
 
 # region exclude-system
-with client.subscribe_to_all(filter_exclude=r"\$.+") as subscription:
-    for event in subscription:
-        handle_event(event)
+async with await client.subscribe_to_all(
+    filter_exclude=r"\$.+"
+) as subscription:
+    async for event in subscription:
+        await handle_event(event)
 # endregion exclude-system
 
 
 # region event-type-prefix
-with client.subscribe_to_all(
+async with await client.subscribe_to_all(
     filter_include=[r"customer-.*"],
 ) as subscription:
-    for event in subscription:
-        handle_event(event)
+    async for event in subscription:
+        await handle_event(event)
 # endregion event-type-prefix
 
 
@@ -68,22 +72,22 @@ event_data = NewEvent(
     data=b'{"id": "1", "important_data": "some value"}',
 )
 
-client.append_to_stream(
+await client.append_to_stream(
     stream_name="user-" + str(uuid4()),
     current_version=StreamState.ANY,
     events=event_data,
 )
 
 # region stream-prefix
-with client.subscribe_to_all(
+async with await client.subscribe_to_all(
     filter_by_stream_name=True,
     filter_include=[r"user-.*"],
 ) as subscription:
-    for event in subscription:
-        handle_event(event)
+    async for event in subscription:
+        await handle_event(event)
 # endregion stream-prefix
 
-client.append_to_stream(
+await client.append_to_stream(
     stream_name="account-stream",
     current_version=StreamState.ANY,
     events=event_data,
@@ -91,46 +95,46 @@ client.append_to_stream(
 
 
 # region stream-regex
-with client.subscribe_to_all(
+async with await client.subscribe_to_all(
     filter_by_stream_name=True,
     filter_include=["account.*", "savings.*"],
 ) as subscription:
-    for event in subscription:
-        handle_event(event)
+    async for event in subscription:
+        await handle_event(event)
 # endregion stream-regex
 
 # region checkpoint
 # get last recorded commit position
 recorded_position = 0
 
-with client.subscribe_to_all(
+async with await client.subscribe_to_all(
     commit_position=recorded_position,
     filter_by_stream_name=True,
     filter_include=["account.*", "savings.*"],
     include_checkpoints=True,
 ) as subscription:
-    for event in subscription:
+    async for event in subscription:
 
-        # checkpoints have a commit positions
+        # checkpoints have commit positions
         if isinstance(event, Checkpoint):
             print("We got a checkpoint!", event.commit_position)
         else:
             print("We got an event!", event.commit_position)
 
         # record commit position
-        handle_event(event)
+        await handle_event(event)
 # endregion checkpoint
 
 
 print("region checkpoint-with-interval")
 # region checkpoint-with-interval
-with client.subscribe_to_all(
+async with await client.subscribe_to_all(
     commit_position=recorded_position,
     include_checkpoints=True,
     checkpoint_interval_multiplier=5,
 ) as subscription:
-    for event in subscription:
-        handle_event(event)
+    async for event in subscription:
+        await handle_event(event)
 # endregion checkpoint-with-interval
 
-client.close()
+await client.close()
--- samples_async/subscribing_to_stream.py
+++ samples_async/subscribing_to_stream.py
@@ -1,150 +1,150 @@
-# ruff: noqa: S106
-from time import sleep
+# ruff: noqa: S106, F704, PLE1142
+import asyncio
+import sys
 from typing import Optional
 from uuid import uuid4
 
 from kurrentdbclient import (
-    KurrentDBClient,
+    AsyncCatchupSubscription,
+    AsyncKurrentDBClient,
     NewEvent,
     StreamState,
 )
 from kurrentdbclient.exceptions import ConsumerTooSlowError
-from kurrentdbclient.streams import CatchupSubscription, RecordedEvent
+from kurrentdbclient.streams import RecordedEvent
 from tests.test_client import get_server_certificate
 
 DEBUG = False
-_print = print
-
 
 def print(*args):  # noqa: A001
     if DEBUG:
-        _print(*args)
+        sys.stdout.write(" ".join([repr(arg) for arg in args]) + "\n")
 
 
 KDB_TARGET = "localhost:2114"
 qs = "MaxDiscoverAttempts=2&DiscoveryInterval=100&GossipTimeout=1"
 
-client = KurrentDBClient(
+client = AsyncKurrentDBClient(
     uri=f"kdb://admin:changeit@{KDB_TARGET}?{qs}",
     root_certificates=get_server_certificate(KDB_TARGET),
 )
+await client.connect()
 
 stream_name = "MyStreamType-" + str(uuid4())
 
-client.append_to_stream(
+await client.append_to_stream(
     stream_name,
-    events=[
-        NewEvent(type="test-event", data=b"{}") for _ in range(5)
-    ],
+    events=[NewEvent(type="test-event", data=b"{}") for _ in range(5)],
     current_version=StreamState.ANY,
 )
 
 
-subscription: CatchupSubscription
+subscription: AsyncCatchupSubscription
 
 
-def handle_event(ev: RecordedEvent):
+async def handle_event(ev: RecordedEvent):
     print(f"handling event: {ev.stream_position} {ev.type}")
-    subscription.stop()
+    await subscription.stop()
 
 
 # region subscribe-to-stream
-with client.subscribe_to_stream(stream_name) as subscription:
-    for event in subscription:
-        handle_event(event)
+async with await client.subscribe_to_stream(stream_name) as subscription:
+    async for event in subscription:
+        await handle_event(event)
 # endregion subscribe-to-stream
 
 
 # region subscribe-to-all
-with client.subscribe_to_all() as subscription:
-    for event in subscription:
-        handle_event(event)
+async with await client.subscribe_to_all() as subscription:
+    async for event in subscription:
+        await handle_event(event)
 # endregion subscribe-to-all
 
 
 # region subscribe-to-stream-from-position
-with client.subscribe_to_stream(
+async with await client.subscribe_to_stream(
     stream_name=stream_name,
     stream_position=3,
 ) as subscription:
-    for event in subscription:
-        handle_event(event)
+    async for event in subscription:
+        await handle_event(event)
 # endregion subscribe-to-stream-from-position
 
+
 # region subscribe-to-all-from-position
-with client.subscribe_to_all(
+async with await client.subscribe_to_all(
     commit_position=0,
 ) as subscription:
-    for event in subscription:
-        handle_event(event)
+    async for event in subscription:
+        await handle_event(event)
 # endregion subscribe-to-all-from-position
 
 
 # region subscribe-to-stream-live
-with client.subscribe_to_stream(
+async  with await client.subscribe_to_stream(
     stream_name=stream_name,
     from_end=True,
 ) as subscription:
 
     # Elsewhere, append new events...
-    commit_position = client.append_to_stream(
-        stream_name=stream_name,
+    commit_position = await client.append_to_stream(
+        stream_name,
         events=NewEvent(type="MyEventType", data=b"{}"),
         current_version=StreamState.ANY,
     )
 
     # Receive new events...
-    for event in subscription:
-        handle_event(event)
+    async for event in subscription:
+        await handle_event(event)
 # endregion subscribe-to-stream-live
 
 
 # region subscribe-to-all-live
-with client.subscribe_to_all(from_end=True) as subscription:
+async with await client.subscribe_to_all(
+    from_end=True
+) as subscription:
 
     # Elsewhere, append new events...
-    commit_position = client.append_to_stream(
-        stream_name=stream_name,
+    commit_position = await client.append_to_stream(
+        stream_name,
         events=NewEvent(type="MyEventType", data=b"{}"),
         current_version=StreamState.ANY,
     )
 
     # Receive new events...
-    for event in subscription:
-        handle_event(event)
+    async for event in subscription:
+        await handle_event(event)
 # endregion subscribe-to-all-live
 
 
 # Allow time for system projection to process event.
-sleep(1)
+await asyncio.sleep(1)
 # Check there is a "$et-MyEventType" stream by now.
-events = client.get_stream("$et-MyEventType")
+events = await client.get_stream("$et-MyEventType")
 
 
 # region subscribe-to-stream-resolving-linktos
-with client.subscribe_to_stream(
+async with await client.subscribe_to_stream(
     stream_name="$et-MyEventType",
     resolve_links=True,
 ) as subscription:
-    for event in subscription:
-        handle_event(event)
+    async for event in subscription:
+        await handle_event(event)
 # endregion subscribe-to-stream-resolving-linktos
 
-
-def get_recorded_stream_position() -> Optional[int]:  # noqa: FA100
+async def get_recorded_stream_position() -> Optional[int]:  # noqa: FA100
     return None
-
 
 # region subscribe-to-stream-subscription-dropped
 while True:
-    with client.subscribe_to_stream(
+    async with await client.subscribe_to_stream(
         stream_name=stream_name,
-        stream_position=get_recorded_stream_position(),
+        stream_position=await get_recorded_stream_position(),
     ) as subscription:
         try:
-            for event in subscription:
+            async for event in subscription:
                 # record stream position
-                handle_event(event)
+                await handle_event(event)
 
         except ConsumerTooSlowError:
             # subscription was dropped
@@ -153,19 +153,18 @@ while True:
     break
 
 
-def get_recorded_commit_position() -> Optional[int]:  # noqa: FA100
+async def get_recorded_commit_position() -> Optional[int]:  # noqa: FA100
     return 0
-
 
 # region subscribe-to-all-subscription-dropped
 while True:
-    with client.subscribe_to_all(
-        commit_position=get_recorded_commit_position(),
+    async with await client.subscribe_to_all(
+        commit_position=await get_recorded_commit_position(),
     ) as subscription:
         try:
-            for event in subscription:
+            async for event in subscription:
                 # record commit position
-                handle_event(event)
+                await handle_event(event)
 
         except ConsumerTooSlowError:
             # subscription was dropped
@@ -180,21 +179,21 @@ credentials = client.construct_call_credentials(
     password="changeit",
 )
 
-with client.subscribe_to_all(
+async with await client.subscribe_to_all(
     credentials=credentials,
 ) as subscription:
-    for event in subscription:
-        handle_event(event)
+    async for event in subscription:
+        await handle_event(event)
 # endregion overriding-user-credentials
 
 
 # region stream-prefix-filtered-subscription
-with client.subscribe_to_all(
+async with await client.subscribe_to_all(
     filter_include=[r"MyStreamType-.*"],
     filter_by_stream_name=True,
 ) as subscription:
-    for event in subscription:
-        handle_event(event)
+    async for event in subscription:
+        await handle_event(event)
 # endregion stream-prefix-filtered-subscription
 
-client.close()
+await client.close()
